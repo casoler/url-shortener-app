861332b7fa63b301f473f530a1c9c00eeedf6d92
casoler <casoler11@gmail.com>
2016-07-31 17:27:45 -0500

    links model and controler

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,22 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.create(
+      slug: params[:slug],
+      target_url: params[:target_url])
+
+    if @link.save
+      redirect_to :index
+    else
+      flash[:notice] = 'Oops!  The link was not saved!  Try again.'
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-   authenticated :user do
-     root 'links#index', as: :authenticated_root
-   end
+    authenticated :user do
+      root 'links#index', as: :authenticated_root
+    end
 
-   unauthenticated do
-     root 'devise/registrations#new', as: :unauthenticated_root
-   end
- end
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  get '/links' => "links#index"
+  get '/links/new' => 'links#new'
+  post '/links' => 'links#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
